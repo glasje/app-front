ba0b98f9c71195cc4227fe02047addae0d7074ff
Franco David <franco.avalos@live.com>
2018-12-16 00:12:47 -0300

    Eliminar listas y Agregar enlaces en el preview

--- src/app/components/list-modules/list-modules.component.ts
+++ src/app/components/list-modules/list-modules.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigurarAppsService } from 'src/app/services/configurar-apps.service';
 import { DatosApps } from 'src/app/models/DatosApps';
+import { element } from '@angular/core/src/render3/instructions';
+import { BoundAttribute } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-list-modules',
@@ -9,17 +11,45 @@ import { DatosApps } from 'src/app/models/DatosApps';
 })
 
 export class ListModulesComponent implements OnInit {
-  listaCaract:DatosApps;
+  listaModulos:DatosApps;
+  listaCaracteristicas:any[] = [];
 
   constructor(private _configuracionApps:ConfigurarAppsService) {
-    this.listaCaract = new DatosApps();
+    this.listaModulos = new DatosApps();
   }
 
   ngOnInit() {
     this._configuracionApps.change.subscribe(data => {
-      this.listaCaract = this._configuracionApps.listaDatos;
-      console.log( this.listaCaract);
+      this.listaModulos = this._configuracionApps.listaDatos;
+      console.log( this.listaModulos);
+      this.joinData(this.listaModulos);
     });
   }
+
+  joinData(lista){
+    let array_extract = [];
+    this.listaCaracteristicas = [];
+    
+    array_extract.push(lista.contactoUsuario);
+    array_extract.push(lista.listaFacebook);
+    array_extract.push(lista.listaGoogle);
+    array_extract.push(lista.listaHtml);
+    array_extract.push(lista.listaPdf);
+    array_extract.push(lista.listaRadio);
+    array_extract.push(lista.listaTwitter);
+    array_extract.push(lista.listaWordPress);
+    array_extract.push(lista.listaYoutube);
+
+    array_extract.forEach(modulos => {
+      modulos.forEach(atributos => {
+          this.listaCaracteristicas.push(atributos);
+      });
+    });
+  }
+
+  EliminarModulos(item){
+    this._configuracionApps.EliminarDatos(item);
+  }
+
 
 }
--- src/app/components/preview-phone/preview-phone.component.ts
+++ src/app/components/preview-phone/preview-phone.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosApps } from 'src/app/models/DatosApps';
+import { ConfigurarAppsService } from 'src/app/services/configurar-apps.service';
 
 @Component({
   selector: 'app-preview-phone',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview-phone.component.css']
 })
 export class PreviewPhoneComponent implements OnInit {
+  listaModulos:DatosApps;
 
-  constructor() { }
+  constructor(private _configuracionApps:ConfigurarAppsService)   {
+    this.listaModulos = new DatosApps();
+  }
 
   ngOnInit() {
+    this._configuracionApps.change.subscribe(data => {
+      this.listaModulos = this._configuracionApps.listaDatos;
+      this.agregarModulo(this.listaModulos);
+    });
   }
   openSideBar() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -17,5 +26,63 @@ export class PreviewPhoneComponent implements OnInit {
   closeSideBar(){
     document.getElementById("mySidenav").style.width = "0";
   }
+  agregarModulo(lista){
+    let elements = document.getElementsByClassName('a-modulo');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+    let array_mods = []
+    if(lista.contactoUsuario.length > 0){
+      array_mods.push(lista.contactoUsuario);
+    }
+    if(lista.listaFacebook.length > 0){
+      array_mods.push(lista.listaFacebook);
+    }
+    if(lista.listaGoogle.length > 0){
+      array_mods.push(lista.listaGoogle);
+    }
+    if(lista.listaHtml.length > 0){
+      array_mods.push(lista.listaHtml);
+    }
+    if(lista.listaPdf.length > 0){
+      array_mods.push(lista.listaPdf);
+    }
+    if(lista.listaTwitter.length > 0){
+      array_mods.push(lista.listaTwitter);
+    }
+    if(lista.listaWordPress.length > 0){
+      array_mods.push(lista.listaWordPress);
+    }
+    if(lista.listaYoutube.length > 0){
+      array_mods.push(lista.listaYoutube);
+    }
+    if(lista.listaRadio.length > 0){
+      array_mods.push(lista.listaRadio);
+    }
+    this.AgregarEnlaceMod(array_mods);
+  }
+
+  AgregarEnlaceMod(mod){
+    let sidenav = document.getElementById("mySidenav");
+    mod.forEach(element => {
+      console.log(element);
+      element.forEach(atributos =>{
+        var enlace = document.createElement("a");
+        enlace.setAttribute('href', '#dos');
+        enlace.classList.add('a-modulo');
+        enlace.style.padding = '8px 8px 8px 32px';
+        enlace.style.textDecoration = 'none';
+        enlace.style.fontSize = '25px';
+        enlace.style.color = '#818181';
+        enlace.style.display = 'block';
+        enlace.style.transition = '0.3s'; 
+        enlace.innerHTML = atributos.nombre;
+        sidenav.appendChild(enlace);
+      })      
+    });
+  }
+
+
+
 
 }
