6f5dd6544ff4f737b15c373050e4ba9edf21998a
Franco David <franco.avalos@live.com>
2018-12-15 21:37:36 -0300

    ver data listada

--- src/app/components/list-modules/list-modules.component.ts
+++ src/app/components/list-modules/list-modules.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigurarAppsService } from 'src/app/services/configurar-apps.service';
+import { DatosApps } from 'src/app/models/DatosApps';
 
 @Component({
   selector: 'app-list-modules',
   templateUrl: './list-modules.component.html',
   styleUrls: ['./list-modules.component.css']
 })
-export class ListModulesComponent implements OnInit {
 
-  constructor() { }
+export class ListModulesComponent implements OnInit {
+  listaCaract:DatosApps;
+
+  constructor(private _configuracionApps:ConfigurarAppsService) {
+    this.listaCaract = new DatosApps();
+  }
 
   ngOnInit() {
+    this._configuracionApps.change.subscribe(data => {
+      this.listaCaract = this._configuracionApps.listaDatos;
+      console.log( this.listaCaract);
+    });
   }
 
 }
